572669d2199f1ffc9209fbba890f61d57fd644e8
Ricoshallow <2297946635@qq.com>
2021-06-21 18:40:32 +0800

    request start

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+import request from '../../utils/request'
+
 // pages/index/index.js
 Page({
 
@@ -11,8 +13,10 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: async function (options) {
+    let result = await request('/banner',{type: 2})
+    console.log(result);
+    
   },
 
   /**
---
+++ utils/request.js
@@ -0,0 +1,30 @@
+//发送Ajax请求
+/**
+ * 1.封装功能函数
+ * 
+ * 2.封装功能组件
+ */
+
+import config from './config'
+
+export default (url, data={}, method='GET') => {
+    // new Promise初始化实例的状态为pending
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: config.host + url,
+            data,
+            method,
+            success: (res)=>{
+                console.log('请求成功',res);
+                // 修改promise的状态为成功状态resolved
+                resolve(res.data)
+            },
+            fail: (err)=>{
+                console.log('请求失败',err);
+                // 修改promise的状态为失败状态rejected
+                reject(err)
+            }
+        });
+    })
+    
+}
