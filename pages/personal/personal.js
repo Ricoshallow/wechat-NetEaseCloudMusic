59c1cf6fcbb741673247deaa7a85233fc6228f99
Ricoshallow <2297946635@qq.com>
2021-06-22 14:10:09 +0800

    personal page transform and transition

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,19 +1,50 @@
 // pages/personal.js
+let startY = 0
+let moveY =0
+let movedY =0
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform: 'translateY(0)',
+    coverTransition: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
+
+  handleTouchStart(event){
+    startY = event.touches[0].clientY
+    this.setData({
+      coverTransition: ''
+    })
+  },
+  handleTouchMove(event ){
+    moveY = event.touches[0].clientY
+    movedY = moveY - startY
+    // console.log(movedY);
+    if (movedY<=0){
+      return
+    }
+    if (movedY>=80){
+      movedY = 80
+    }
+    this.setData({
+      coverTransform: `translateY(${movedY}rpx)`
+    })
+  },
+  handleTouchEnd(){
+    this.setData({
+      coverTransform: 'translateY(0)',
+      coverTransition: 'transform 1s linear'
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
